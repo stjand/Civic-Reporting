a81a82cfbac42b9539e98ae32553566b16727b7e
stjand <surya567teja@gmail.com>
2025-09-23 11:10:28 +0530

    set to hyd 2

--- frontend/src/components/MapPicker.jsx
+++ frontend/src/components/MapPicker.jsx
@@ -24,7 +24,7 @@ const MapPicker = ({ onLocationSelect, initialLocation = null, isFullscreen = fa
 
     try {
       // Initialize map
-      const map = L.map(mapRef.current).setView([17.4116, 78.4795], 20)
+      const map = L.map(mapRef.current).setView([17.4116, 78.4795], 7)
       mapInstanceRef.current = map
 
       // Add tile layer
